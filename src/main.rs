9206d8f1a2db39f69b93382ce9594e6ae359b7f8
gifnksm <makoto.nksm+github@gmail.com>
2019-04-07 16:12:22 +0900

    Setup freestanding rust binary

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+#![allow(clippy::empty_loop)]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
