cde6961fcf65dfe173e24ebeb305e1850a33c645
gifnksm <makoto.nksm+github@gmail.com>
2019-04-07 20:48:29 +0900

    Add vga_buffer module

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,16 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    loop {}
+    println!("Hello world{}", "!");
+    panic!("Some panic message!");
 }
