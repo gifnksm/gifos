480681f3988024f5df9678b5b26c19301d29e427
gifnksm <makoto.nksm+github@gmail.com>
2019-07-20 13:52:22 +0900

    Run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,10 @@ pub extern "C" fn _start() -> ! {
 
     use x86_64::registers::control::Cr3;
     let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    println!(
+        "Level 4 page table at: {:?}",
+        level_4_page_table.start_address()
+    );
 
     let ptr = 0xdeadbeaf as *mut u32;
     unsafe {
